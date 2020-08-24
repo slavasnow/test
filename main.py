768fd2b1854e2b6d8bf2bb69ce89a4d3456787b4
slavasnow <51907565+slavasnow@users.noreply.github.com>
2020-08-24 16:57:13 +0300

    new int

--- main.py
+++ main.py
@@ -7,17 +7,29 @@ word_first = 'Panel' #искомое слово
 word_succes = 'Succes' # дополнительный параметр
 file = 'Authentication.log'
 
+def main():
+    R = input('режим работы программы : ')
+    if R == 'succes':
+        succes_auto()
+    elif R == 'users':
+        users()
+
+
 #удачные попытки авторизации кто удачно авторизовался на принтере.
-with io.open(file) as f:
-    for line in f:
-        if word_succes in line:
-            print (line, end = "")
+def succes_auto():
+    with io.open(file) as f:
+        for line in f:
+            if word_succes in line:
+                print (line, end = "")
             
 
 #Все кто был авторизован на принтере хоть раз
+def users():
+    with io.open(file) as f:
+        for line in f:
+            if word_first in line:
+                #print (line, end="")
+                print (re.findall('[a-z]{5,}', line))
 
-with io.open(file) as f:
-    for line in f:
-        if word_first in line:
-            #print (line, end="")
-            print (re.findall('[a-z]{5,}', line))
+if __name__ == "__main__":
+    main()
